65d808c1b14ec4193124bc824859ed05baa8a1e0
Chad Smith <Smith.Chad12@gmail.com>
2017-03-24 01:19:02 -0500

    Updates to module loading
    -Fix the mispelling for platformModules
    -Added comments to clarify order of modules being loaded in

--- XamarinBase/XamarinBase/App.xaml.cs
+++ XamarinBase/XamarinBase/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Ninject;
+﻿using Ninject;
 using Ninject.Modules;
 using Xamarin.Forms;
 using XamarinBase.Modules;
@@ -23,16 +19,17 @@ namespace XamarinBase
         /// <summary>
         /// Initializes a new instance of the app.
         /// </summary>
-        /// <param name="platofrmModules">The platoform specific modules that need to be loaded into the kernal.</param>
-        public App(params INinjectModule[] platofrmModules)
+        /// <param name="platformModules">The platoform specific modules that need to be loaded into the kernal.</param>
+        public App(params INinjectModule[] platformModules)
         {
             InitializeComponent();
-            // we must put some page, even if the page is nothing for iOS
+            // we must put some page, even if the page is nothing, for iOS
             MainPage = new ContentPage(); 
 
             Kernal = new StandardKernel();
             // Register all the modules with the kernal
-            Kernal.Load(platofrmModules);
+            // We register the platform specific modules first because they have the bindings to the IDatabase which the ServiceModule will need.
+            Kernal.Load(platformModules);
             Kernal.Load(new ServiceModule(), new ViewModelModule(), new NavigationModule(MainPage.Navigation));
         }
 
